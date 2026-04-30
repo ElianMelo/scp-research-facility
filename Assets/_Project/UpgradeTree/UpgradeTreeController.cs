2553973786121b62dab42922f8515c267615db70
Elian Melo <elianmelomorais@gmail.com>
2026-04-29 21:07:47 -0300

    Imported SCP 939

---
+++ Assets/_Project/UpgradeTree/UpgradeTreeController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public enum UpgradeType
+{
+    None,
+    Capacity,
+    Recharge,
+    Eficiency,
+    Speed,
+    Cooldown,
+    Range,
+}
+
+public enum UpgradeTarget
+{
+    None,
+    Battery,
+    Security,
+    SCP01,
+    SCP02,
+    SCP03,
+    SCP04,
+    Radioactive,
+    Chemical,
+    Thermic,
+    Phonetic
+}
+
+
+public class UpgradeTreeController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
